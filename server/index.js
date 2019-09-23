9f7472be875d788b6e6bad33ee82f7436c582bd0
Brandon Turner <bturner.1999@yahoo.com>
2019-09-23 12:10:54 -0400

    nodemon

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello New Paltz!'));
 
 app.get('/heb', function(req, res){
             res.send('Shalom World!')
