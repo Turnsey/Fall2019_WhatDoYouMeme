2830a78bbbc5ed75ab3b29da61e9c45ac146177d
Brandon Turner <bturner.1999@yahoo.com>
2019-09-23 12:03:08 -0400

    Express example

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/heb', function(req, res){
+            res.send('Shalom World!')
+    } 
+)
+
+app.listen(port, () => console.log(`Running on http://localhost:${port}`));
