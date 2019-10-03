0430a1cc45cff478a944bac82936b4c5f0cfd3db
Brandon Turner <bturner.1999@yahoo.com>
2019-10-03 12:15:22 -0400

    More Express Practice

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
+const userController = require('./controllers/Users');
 const app = express();
 const port = 3000;
-const path = require('path')
 
 app.get('/', (req, res) => res.send('Hello New Paltz!'));
+
+app.use( function(req, res, next){
+    //logging
+    console.log( {params: req.params, body: req.body, url: req.url, query: req.query, headers: req.headers});
+    next();
+})
 
 app.use('/static', express.static(path.join(__dirname, '../NoFramework')))
 
@@ -11,5 +18,7 @@ app.get('/heb', function(req, res){
             res.send('Shalom World!')
     } 
 )
+
+app.use('/users', userController);
 
 app.listen(port, () => console.log(`Running on http://localhost:${port}`));
---
+++ server/models/Users.js
@@ -0,0 +1,6 @@
+const users = [
+    {name: "Brandon", age: 20},
+    {name: "Donald", age: 98}
+];
+
+module.exports = users;
