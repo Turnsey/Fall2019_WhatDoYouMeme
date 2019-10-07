c8a2e3144f6c617709d40f8bd9313b0c5bd37879
Brandon Turner <bturner.1999@yahoo.com>
2019-10-07 12:17:39 -0400

    Controller tweaking

--- server/index.js
+++ server/index.js
@@ -1,24 +1,13 @@
 const express = require('express');
 const path = require('path');
 const userController = require('./controllers/Users');
+const gameController = require('./controllers/Game');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello New Paltz!'));
-
-app.use( function(req, res, next){
-    //logging
-    console.log( {params: req.params, body: req.body, url: req.url, query: req.query, headers: req.headers});
-    next();
-})
-
 app.use('/static', express.static(path.join(__dirname, '../NoFramework')))
 
-app.get('/heb', function(req, res){
-            res.send('Shalom World!')
-    } 
-)
-
-app.use('/users', userController);
+app.use('/users', userController)
+app.use('/game', gameController);
 
 app.listen(port, () => console.log(`Running on http://localhost:${port}`));
