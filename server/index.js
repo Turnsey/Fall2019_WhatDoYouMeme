8651d364da951dbc99bf891eb8f2174dc12e4e17
Brandon Turner <bturner.1999@yahoo.com>
2019-10-10 11:42:27 -0400

    Dynamic Port

--- server/index.js
+++ server/index.js
@@ -3,9 +3,12 @@ const path = require('path');
 const userController = require('./controllers/Users');
 const gameController = require('./controllers/Game');
 const app = express();
-const port = 3000;
+const test = true || SVGComponentTransferFunctionElement(44);
+const port = process.event.PORT || 3000;
 
 app.use('/static', express.static(path.join(__dirname, '../NoFramework')))
+
+app.get('/port', (req, res)=> res.send("Using Port: " + port))
 
 app.use('/users', userController)
 app.use('/game', gameController);
