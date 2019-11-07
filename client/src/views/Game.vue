df3905f088eca23ae1fa4c96bbabdf6e3a14e8b8
Brandon Turner <bturner.1999@yahoo.com>
2019-11-07 12:15:50 -0500

    Tweaks/Server-Side Stuff

--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -1,71 +1,70 @@
 <template>
 <div>
-  <h1 class="is-size-1">
-      This is the Game Page
-  </h1>
+    <h1 class="is-size-1">
+        This is the Game Page
+    </h1> 
 
-  <div class="columns">
-    <div class="column-is-one-quarter">
-      
-      <ul class="panel">
-          <p class="panel-heading">
-            Players
-          </p>
-          <li v-for="(p,i) in game.Players" :key="i" class="panel-block" :class="{ 'is-active': i == game.Dealer }">
-              <span class="panel-icon">
-                <i class="fas " :class=" i ==game.Dealer ? 'fa-user-secret' : 'fa-user' " aria-hidden="true"></i>
-              </span>
-              {{p.name}}
-          </li>
-      </ul>
-          <ul class="panel">
-          <p class="panel-heading">
-            My Hand
-          </p>
-          <li v-for="(c,i) in My_Captions" :key="i" class="panel-block is-active">
-              <span class="panel-icon">
-                {{c}}
-              </span>
-              {{p.name}}
-          </li>
-      </ul>
+    <div class="columns">
+        <div class="column is-one-quarter">
+            
+            <ul class="panel">
+                <p class="panel-heading">
+                    Players
+                </p>
+                <li v-for="(p, i) in game.Players " :key="i" 
+                    class="panel-block" :class="{ 'is-active': i == game.Dealer }">
+                    <span class="panel-icon">
+                    <i class="fas" :class=" i == game.Dealer ? 'fa-user-secret' : 'fa-user' " aria-hidden="true"></i>
+                    </span>
+                    {{p.name}}
+                </li>
+            </ul>
+
+            <ul class="panel">
+                <p class="panel-heading">
+                    My Hand
+                </p>
+                <li v-for="(c, i) in My_Captions " :key="i" class="panel-block is-active">
+                    {{c}}
+                </li>
+            </ul>
+
+        </div>
+        <div class="column">
+            <div class="box is-clickable" @click="pictureClicked">
+                <img    alt="Current Picture in Play" class="image is-fullwidth"
+                        :src="game.Picture_In_Play" v-if="game.Picture_In_Play"  />
+                <div class="notification is-primary" v-else>
+                    Flip First Picture
+                </div>
+            </div>
+        </div>
     </div>
-    </div>
-    <div class="column">
-      <div class="box is-clickable" @click="pictureClicked">
-          <img alt="Picture in Play" :src="game.Picture_In_Play" v-if="game.Picture_In_Play" class="box"/>
-          <div class="notification-is-primary">
 
-          </div>
-      </div>
-  </div>
-
-  
 </div>
 </template>
 
 <script>
-import { Game_Client, Game_Server, My_Captions } from "../models/Game";
-
+import { Game_Server } from "../models/Game";
 export default {
     data: ()=> ({
-        game: Game_Client,
-        My_Captions
+        game: {},
+        My_Captions: []
     }),
-    created(){
-      this.My_Captions = Game_Server.Get_Hand();
+    async created(){
+        this.My_Captions = await Game_Server.Get_Hand();
+        this.game = await Game_Server.Get_State();
     },
     methods: {
-      pictureClicked(){
-        this.game.Picture_In_Play = Game_Server.Get_Next_Picture();
-        this.game.Dealer ++;
+        pictureClicked(){
+            Game_Server.Flip_Picture();
         }
     }
 }
 </script>
 
 <style>
-    .is-clickable{
-      cursor: pointer;
+    .is-clickable {
+        cursor: pointer;
     }
 </style>
